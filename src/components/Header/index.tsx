c9d86d4d4d62a22bab5ed41ca7a403ab13aaabe2
HerbertSousa <herberti1405@outlook.com>
2021-04-24 12:30:36 -0300

    responsive Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, useBreakpointValue, IconButton, Icon } from '@chakra-ui/react';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
+import { RiMenuLine } from 'react-icons/ri';
 
 export function Header() {
+    const { onOpen } = useSidebarDrawer();
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -21,6 +25,15 @@ export function Header() {
             px="6"
             align="center"
         >
+            { !isWideVersion && (
+                <IconButton 
+                    icon={<Icon as={RiMenuLine}/>}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr={2}
+                />
+            )}
             <Logo />
             { isWideVersion && <SearchBox />}
             <Flex
