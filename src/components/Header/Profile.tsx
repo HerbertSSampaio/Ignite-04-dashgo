4141adbfb5acf6156e74e58226642e223de4cd43
HerbertSousa <herberti1405@outlook.com>
2021-04-24 11:48:03 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps{
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }:ProfileProps) {
     return (
         <Flex align="center">
+            { showProfileData && (
             <Box mr="4" textAlign="right">
                 <Text>Herbert</Text>
                 <Text color="gray.300" fontSize="small">
                     herbert@gmail.com
                 </Text>
             </Box>
+            )}
 
             <Avatar size="md" name="Herbert De Sousa" src="https://avatars.githubusercontent.com/u/49881040"/>
         </Flex>
